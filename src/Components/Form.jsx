e8c94db3e8124176b01904390b83added10b3aa1
[saurabh singh] <[krider240@gmail.com]>
2025-02-17 00:21:31 +0530

    Changed support buttons to input type buttons to eliminate there triggered submit event

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -24,10 +24,16 @@ const Form = () => {
     <form onSubmit={submithandler} className='form'>
   <div className='formDetails'>
   <div className='connectBtn'>
-        <button className='call'>Via Support Chat</button>
-        <button className='chat'>Via Call</button>
+        {/* <button className='call'>Via Support Chat</button>
+         */}
+         <input type="button" className='call' value="Via Support Chat" />
+        {/* <button className='chat'>Via Call</button> */}
+        <input type="button" className='chat' value="Via Call" />
     </div>
-    <div className='emailBtn' ><button className='email'>Via Email Form</button></div>
+    <div className='emailBtn' >
+      {/* <button className='email'>Via Email Form</button> */}
+      <input type="button" className='email' value="Via Email Form" />
+      </div>
     <div className='inputs'>
         <input type="text" onChange={changeHandler} name="fname" id="name" placeholder='Enter Full Name' /> <br />
         <input type="text" onChange={changeHandler} name="email" id="email" placeholder='Enter Email Address' /> <br />

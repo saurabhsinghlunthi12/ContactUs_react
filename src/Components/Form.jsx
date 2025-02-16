3424eb20d75364c17c184e20574e7d6c8660aa05
[saurabh singh] <[krider240@gmail.com]>
2025-02-16 22:41:28 +0530

    Added useState and changeHandler function to get and console user submitted details

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Form.css"
 
 const Form = () => {
+  const [formData,setFormData]=useState({fname:"",email:"",comment:""});
+  //function to update formData
+  function changeHandler(event){
+    setFormData(prev=>{
+      return{...prev,
+        [event.target.name]:event.target.value
+      }
+    }
+      
+    )
+  };
+
+   // function to console submitted details by user
+  function submithandler(event){
+    event.preventDefault();
+    console.log(formData);
+  }
+
   return (
-    <div className='form'>
+    <form onSubmit={submithandler} className='form'>
   <div className='formDetails'>
   <div className='connectBtn'>
         <button className='call'>Via Support Chat</button>
@@ -11,13 +29,13 @@ const Form = () => {
     </div>
     <div className='emailBtn' ><button className='email'>Via Email Form</button></div>
     <div className='inputs'>
-        <input type="text" name="name" id="name" placeholder='Enter Full Name' /> <br />
-        <input type="text" name="email" id="email" placeholder='Enter Email Address' /> <br />
-        <textarea type="text" name="comment" id="comment" placeholder='Type your query' /><br />
+        <input type="text" onChange={changeHandler} name="fname" id="name" placeholder='Enter Full Name' /> <br />
+        <input type="text" onChange={changeHandler} name="email" id="email" placeholder='Enter Email Address' /> <br />
+        <textarea type="text" onChange={changeHandler} name="comment" id="comment" placeholder='Type your query' /><br />
     </div>
     <div className='submitDiv'><button className='submitBtn'>Submit</button></div>
     </div>
-  </div>
+  </form>
   )
 }
 
